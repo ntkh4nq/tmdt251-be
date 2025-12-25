cb34fc672654db197792bdd0f2a198d62d98c85d
binhnguyensib <binhnguyen20604@gmail.com>
2025-12-26 00:37:48 +0700

    update email service

--- be/api/app/services/email_service.py
+++ be/api/app/services/email_service.py
@@ -53,8 +53,7 @@ class EmailService:
             message.attach(part2)
             
             # Send email
-            with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
-                server.starttls()
+            with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as server:
                 server.login(self.smtp_username, self.smtp_password)
                 server.send_message(message)
             
